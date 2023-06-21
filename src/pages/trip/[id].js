831c3642673a80ae6c4dd8705d4a9a90548140c2
LunaCodee <laura.zumbakiene@gmail.com>
2023-06-21 09:20:38 +0300

    id

--- src/pages/trip/[id].js
+++ src/pages/trip/[id].js
@@ -2,62 +2,48 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
 import styles from "./styles.module.css";
-import Navbar from "../components/Navbar/Navbar";
-import Footer from "../components/Footer/Footer";
+import Navbar from "../../components/Navbar/styles.module.css";
+import Footer from "../../components/Footer/Footer";
 
-const Trip = () => {
-  //   const [trip, setTrip] = useState();
-  // const router = useRouter();
-  // const fetchEvent = async () => {
-  //   const response = await axios.post(
-  //     `https://643d689d6afd66da6af6326f.mockapi.io/trip/${router.query.id}`,
-  //     { userId: "1234567" }
-  //   );
+const Trip = (trip) => {
+console.log(trip)
+  return (
+    <>
+      {/* <Navbar />
+      <div className={styles.pageWrapper}>
+        {trip && (
+          <div className={styles.wrapper}>
+            <h1>{trip.Destination}</h1>
+            <img className={styles.image} src={trip.ImageUrl} />
 
-  //   const { data } = response;
-  //   setTrip(data);
-  //   console.log("response", response);
-  // };
-
-  // useEffect(() => {
-  //   router.query.id && fetchEvent();
-  // }, [router.query.id]);
-  // return (
-  //   <>
-  //     {/* <Navbar /> */}
-  //     <div className={styles.pageWrapper}>
-  //       {trip && (
-  //         <div className={styles.wrapper}>
-  //           <h1>{trip.Destination}</h1>
-  //           <img className={styles.image} src={trip.ImageUrl} />
-
-  //           <div className={styles.contentWrapper}>
-  //             <div className={styles.info}>
-  //               <h3>{trip.Date}</h3>
-  //               <h3>{trip.Duration}</h3>
-  //             </div>
-  //           </div>
-  //         </div>
-  //       )}
-  //     </div>
-  // <Footer />
-  //   </>
-  // );
+            <div className={styles.contentWrapper}>
+              <div className={styles.info}>
+                <h3>{trip.Date}</h3>
+                <h3>{trip.Duration}</h3>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+  <Footer /> */}
+    </>
+  );
 };
 
-// export async function getServerSideProps(context) {
-//   console.log(context.query.id)
-//   try {
-//     const id = context.query.id;
-//     const response = await axios.post(
-//       `https://643d689d6afd66da6af6326f.mockapi.io/trip/${id}`
-//     );
-//     const { data } = response;
+export async function getServerSideProps(context) {
+  console.log(context.query.id)
+  try {
+    const id = context.query.id;
+    const response = await axios.get(
+      `https://643d689d6afd66da6af6326f.mockapi.io/Trips/${id}`
+    );
+    console.log(response)
+    const { data } = response;
 
-//     return { props: { trip: data }};
+    return { props: { data }};
   
-//   } catch (err) {
-//     console.log("err", err)
-//   }
-// }
+  } catch (err) {
+    console.log("err", err)
+  }
+}
 export default Trip;
